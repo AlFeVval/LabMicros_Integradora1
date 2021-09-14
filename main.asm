dc237a73f3b5e932ce046e5e3047b788f22c0c2d
AlFeVval <88679162+AlFeVval@users.noreply.github.com>
2021-09-13 23:01:45 -0500

    Se agregó mostrar en display, pero tienen error
    Cambiar ceros en camino

--- main.asm
+++ main.asm
@@ -26,7 +26,7 @@
 
 ; CONFIG3H
   CONFIG  CCP2MX = ON           ; CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+  CONFIG  PBADEN = ON          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
   CONFIG  LPT1OSC = OFF         ; Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
   CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
 
@@ -80,27 +80,35 @@ TEMP		EQU	0x50
 TM1		EQU	0x60			;	
 TM2		EQU	0x70			;	
 TM3		EQU	0x80			;
-CONSTANT SDN = 0x3C
-CONSTANT DDN = 0x0A
+CONSTANT D_60 = 0x3C
+CONSTANT D_10 = 0x0A
 ;****************Main code*****************************
     #DEFINE		pausa	    PORTB,7
     #DEFINE		continuar   PORTB,6
     #DEFINE		reinicio    PORTB,5
-    #DEFINE		p_flag	    PORTC,7
-    #DEFINE		c_flag	    PORTC,6
-    #DEFINE		r_flag	    PORTC,5
+    
+    #DEFINE		a_ds	    LATB,4
+    #DEFINE		b_ds	    LATB,3
+    #DEFINE		c_ds	    LATB,2
+    #DEFINE		d_ds	    LATB,1
+    #DEFINE		e_ds	    LATB,0
+    #DEFINE		f_ds	    LATC,7
+    #DEFINE		g_ds	    LATC,6
+    
+    #DEFINE		a_us	    LATD,7
+   
+    
     
 			ORG     0x000             	;reset vector
   			GOTO    MAIN              	;go to the main routine
 INITIALIZE:
+			MOVLW	D'6'
+			MOVWF	ADCON1
+			MOVLW	b'11100000'
 			CLRF	TRISC
-			BCF	p_flag			;Bandera de control para pausa
-			BCF	c_flag			;Bandera de control para continuar
-			BCF	r_flag			;Bandera de control para reset
+			MOVWF	TRISB
+			CLRF	TRISA
 			CLRF	TRISD			;Puerto D como salidas
-			BSF	pausa			;Boton de pausa como entrada
-			BSF	continuar		;Boton de continuar como entrada
-			BSF	reinicio		;Boton de reset como entrada
 			RETURN				;end of initialization subroutine
 
 MAIN:
@@ -108,7 +116,10 @@ MAIN:
 
 BASE:
 			CALL	COUNTDOWN
+			CALL	DELAY_1S
+			CALL	SHOW
 			GOTO 	BASE	    ;infinite loop
+			
 COUNTDOWN:
 			MOVF	TEMP,F			;
 			BZ	VALUE
@@ -118,15 +129,15 @@ COUNTDOWN:
 			CALL	DIVIDE_60
 			CALL	SET_RES_MIN
 			CALL	DIVIDE_10
+			CLRF	COCIENT
 			RETURN				;   
-
 VALUE:
 			MOVLW	d'181'			;
 			MOVWF	TEMP			;
-			GOTO	COUNTDOWN		;			
-
+			GOTO	COUNTDOWN		;	
+			
 DIVIDE_60:
-			MOVLW	SDN
+			MOVLW	D_60
 			SUBWF	RESULTADO
 			MOVF	RESULTADO,F
 			BTFSC	STATUS,C
@@ -141,10 +152,11 @@ INC_COCIENTE_MIN:
 			GOTO	DIVIDE_60
 
 RESIDUO_MIN:
-			MOVLW	SDN
+			MOVLW	D_60
 			ADDWF	RESULTADO,W
 			MOVWF	RESID
 			RETURN
+			
 SET_RES_MIN:
 			MOVF	RESID,W
 			MOVWF	RESULTADO
@@ -152,7 +164,7 @@ SET_RES_MIN:
 			RETURN
     
 DIVIDE_10:
-			MOVLW	DDN
+			MOVLW	D_10
 			subwf	RESULTADO
 			MOVF	RESULTADO,F
 			BTFSC	STATUS,C
@@ -166,10 +178,229 @@ INC_COCIENTE_DS:
 			GOTO	DIVIDE_10
 			
 RESIDUO_DS:
-			MOVLW	DDN
+			MOVLW	D_10
 			ADDWF	RESULTADO,W
 			MOVWF	UNIS
 			RETURN
 			
-	
+
+SHOW:
+			CALL	DISP_MIN
+			CALL	DISP_DECS
+			CALL	DISP_UNIS
+			RETURN
+			
+DISP_MIN:
+			MOVLW	0x00
+			SUBWF	MIN,0	
+			BZ  D0
+			MOVLW	0x01
+			SUBWF	MIN,0
+			BZ  D1
+			MOVLW	0x02
+			SUBWF	MIN,0
+			BZ  D2
+			MOVLW	0x03
+			SUBWF	MIN,0
+			BZ  D3	
+			RETURN
+			
+DISP_DECS:
+			MOVLW	0x00
+			SUBWF	DECS,0	
+			BZ  D0_DS
+			MOVLW	0x01
+			SUBWF	DECS,0
+			BZ  D1_DS
+			MOVLW	0x02
+			SUBWF	DECS,0
+			BZ  D2_DS
+			MOVLW	0x03
+			SUBWF	DECS,0
+			BZ  D3_DS
+			MOVLW	0x04
+			SUBWF	DECS,0
+			BZ  D4_DS
+			MOVLW	0x05
+			SUBWF	DECS,0
+			BZ  D5_DS
+			MOVLW	0x06
+			SUBWF	DECS,0
+			RETURN
+			
+DISP_UNIS:
+			MOVLW	0x00
+			SUBWF	UNIS,0	
+			BZ  D0_US
+			MOVLW	0x01
+			SUBWF	UNIS,0
+			BZ  D1_US
+			MOVLW	0x02
+			SUBWF	UNIS,0
+			BZ  D2_US
+			MOVLW	0x03
+			SUBWF	UNIS,0
+			BZ  D3_US
+			MOVLW	0x04
+			SUBWF	UNIS,0
+			BZ  D4_US
+			MOVLW	0x05
+			SUBWF	UNIS,0
+			BZ  D5_US
+			MOVLW	0x06
+			SUBWF	UNIS,0
+			BZ  D6_US
+			MOVLW	0x07
+			SUBWF	UNIS,0
+			BZ  D7_US
+			MOVLW	0x08
+			SUBWF	UNIS,0
+			BZ  D8_US
+			MOVLW	0x09
+			SUBWF	UNIS,0
+			BZ  D9_US
+			RETURN
+			
+D0:
+			MOVLW	b'01111110'		;//0
+			MOVWF	PORTD			;
+			RETURN;
+D1:
+			MOVLW	b'00110000'		;//1
+			MOVWF	PORTD			;
+			RETURN			;
+D2:
+			MOVLW	b'01101101'		;//2
+			MOVWF	PORTD			;
+			RETURN		;
+D3:
+			MOVLW	b'01111001'		;//3
+			MOVWF	PORTD			;
+			RETURN			;
+			
+D0_DS:
+			BSF	a_ds;
+			BSF	b_ds;
+			BSF	c_ds;
+			BSF	d_ds;
+			BSF	e_ds;
+			BSF	f_ds;
+			BCF	g_ds;
+			RETURN;
+			
+D1_DS:
+			BCF	a_ds;
+			BSF	b_ds;
+			BSF	c_ds;
+			BCF	d_ds;
+			BCF	e_ds;
+			BCF	f_ds;
+			BCF	g_ds;			;
+			RETURN			
+			
+D2_DS:
+			BSF	a_ds;
+			BSF	b_ds;
+			BCF	c_ds;
+			BSF	d_ds;
+			BSF	e_ds;
+			BCF	f_ds;
+			BSF	g_ds;
+			RETURN		
+
+D3_DS:
+			BSF	a_ds;
+			BSF	b_ds;
+			BSF	c_ds;
+			BSF	d_ds;
+			BCF	e_ds;
+			BCF	f_ds;
+			BSF	g_ds;
+			RETURN			
+D4_DS:
+			BCF	a_ds;
+			BSF	b_ds;
+			BSF	c_ds;
+			BCF	d_ds;
+			BCF	e_ds;
+			BSF	f_ds;
+			BSF	g_ds;		;
+			RETURN
+			
+D5_DS:
+			BSF	a_ds;
+			BCF	b_ds;
+			BSF	c_ds;
+			BSF	d_ds;
+			BCF	e_ds;
+			BSF	f_ds;
+			BSF	g_ds;			
+			RETURN
+
+D0_US:
+			MOVLW	b'01111110'		;//0
+			MOVWF	PORTA			;
+			BSF	a_us
+			RETURN;
+D1_US:
+			MOVLW	b'00110000'		;//1
+			MOVWF	PORTA			;
+			BCF	a_us
+			RETURN			;
+D2_US:
+			MOVLW	b'01101101'		;//2
+			MOVWF	PORTA			;
+			BSF	a_us
+			RETURN		;
+D3_US:
+			MOVLW	b'01111001'		;//3
+			MOVWF	PORTA			;
+			BSF	a_us    
+			RETURN			;
+D4_US:
+			MOVLW	b'00110011'		;//4
+			MOVWF	PORTA			;
+			BCF	a_us
+			RETURN			;
+D5_US:
+			MOVLW	b'01011011'		;//5
+			MOVWF	PORTA			;
+			BSF	a_us
+			RETURN;
+D6_US:
+			MOVLW	b'01011111'		;//6
+			MOVWF	PORTA			;
+			BSF	a_us
+			RETURN			;
+D7_US:
+			MOVLW	b'01110000'		;//7
+			MOVWF	PORTA	
+			BSF	a_us
+			RETURN			;
+D8_US:
+			MOVLW	b'01111111'		;//8
+			MOVWF	PORTA	
+			BSF	a_us
+			RETURN;
+D9_US:
+			MOVLW	b'01111011'		;//9
+			MOVWF	PORTA			;
+			BSF	a_us
+			RETURN
+			
+DELAY_1S:		
+    			MOVLW	d'6'			;
+			MOVWF	TM1			;
+			MOVLW	D'235'			;
+		T3:	MOVWF	TM2			;
+		T2:	MOVWF	TM3			;
+		T1:	DECFSZ	TM3			;
+			GOTO	T1		        ;
+			DECFSZ	TM2			;
+			GOTO	T2			;
+			DECFSZ	TM1			;
+			GOTO	T3			;		
+			RETURN
+
+			
 			END
